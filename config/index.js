cae33a92c4cde2a88ee3aa2a348f51b779d655ac
anxiubin <tnqls0120@naver.com>
2022-02-20 15:21:05 +0900

    ♻️ wallet address도 env 에서 관리

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ export const isDevelopment = process.env.NODE_ENV !== 'production';
 //Constants
 export const DEFAULT_QR_CODE = 'DEFAULT';
 export const DEFAULT_ADDRESS = '0x00000000000000000000000000000';
-export const DEPLOY_WALLET_ADDRESS = '0x9bf610E09D53F1A884BECaA43F94a04948285600';
 export const APP_NAME = 'BadgeMeal';
 
 //URL
