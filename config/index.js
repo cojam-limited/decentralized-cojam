04bef66ec4159757a0fe00c21cc268fbdd374d10
anxiubin <tnqls0120@naver.com>
2022-02-20 15:16:26 +0900

    ✨ .env 환경변수 세팅 : dotenv-webpack 패키지 사용

--- config/index.js
+++ config/index.js
@@ -1,2 +1,11 @@
 export const isDevelopment = process.env.NODE_ENV !== 'production';
+
+//Constants
+export const DEFAULT_QR_CODE = 'DEFAULT';
+export const DEFAULT_ADDRESS = '0x00000000000000000000000000000';
+export const DEPLOY_WALLET_ADDRESS = '0x9bf610E09D53F1A884BECaA43F94a04948285600';
+export const APP_NAME = 'BadgeMeal';
+
+//URL
 export const API_BASE_URL = '';
+export const A2P_API_PREPARE_URL = 'https://a2a-api.klipwallet.com/v2/a2a/prepare';
