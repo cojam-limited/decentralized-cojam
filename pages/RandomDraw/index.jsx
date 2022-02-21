b2248cc9a4ad20308839fa9e2e8188552b4d727f
anxiubin <tnqls0120@naver.com>
2022-02-22 01:51:09 +0900

    🚧 RandomDraw페이지에 민팅 함수 테스트용으로 작성

--- pages/RandomDraw/index.jsx
+++ pages/RandomDraw/index.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import Button from '@components/Button';
 import RandomTray from '@assets/img_tray.png';
 import { RandomDrawContainer, Step } from './styles';
-import { addUserMinter } from '@api/UseCaver';
+import { addMinter, removeMinter } from '@api/UseCaver';
+import { mintWithTokenURI } from '@api/UseKaikas';
+import { useWalletData } from '@data/wallet';
 
 function RandomDraw() {
-  const handleClickAddUserMinter = () => {
-    addUserMinter();
+  const { walletData } = useWalletData();
+
+  //테스트용
+  const mint = () => {
+    mintWithTokenURI(13, 'test', 'test2', 'pizza');
+  };
+  const addBadgemealMinter = () => {
+    addMinter(walletData?.account);
+  };
+  const removeBadgemealMinter = () => {
+    removeMinter(walletData?.account);
   };
 
+  const handleClickGetNFT = async () => {
+    /** GetNFT 플로우
+      1. 지갑 연동 여부를 체크한다.
+      2. 지갑이 연동되어 있으면 DB에 저장된 mintData를 조회한다.
+      3. mintData가 있으면 DB에 저장된 하루에 NFT 발급 받은 횟수를 조회한다.
+      4. Owner가 addMinter(유저지갑주소) 호출해서 mint 권한을 유저에게 임시로 준다.
+      5-1. 하루에 NFT 발급 받은 횟수가 3 미만이면 mintWithTokenURI,
+      5-2. 하루에 NFT 발급 받은 횟수가 3 이상이면 mintWithKlay를 호출한다.
+      6. 발행이 완료되면 Owner는 renounceMinter(유저지갑주소)를 호출해서 유저의 mint 권한을 제거한다.
+      7. 발행이 완료되면 지갑주소와 매핑된 mintData, drawResult를 초기화한다.
+    */
+  };
   return (
     <RandomDrawContainer>
       <div className="tray_wrapper">
@@ -27,7 +50,7 @@ function RandomDraw() {
         </Step>
         <Step>
           <span>Step 3</span>
-          <Button text="Get NFT" onClick={handleClickAddUserMinter}></Button>
+          <Button text="Get NFT"></Button>
         </Step>
       </div>
     </RandomDrawContainer>
