de8d654af5196f7e5e4940f6923d741dad7059c2
Sungpyo <yyddp2@naver.com>
2022-02-22 22:21:05 +0900

    :sparkles: KAS API를 통한 소유한 nft 리스트 조회

--- api/UseKAS.js
+++ api/UseKAS.js
@@ -11,15 +11,44 @@ const option = {
 }
 
 export const ownNftList = async (ownaddress) =>{
-
     try{
-        const reponse = await axios.get(`https://kip17-api.klaytnapi.com/v1/contract/${nftcontract}/owner/${ownaddress}`, option);
-        console.log(JSON.stringify(reponse));
-        // return reponse.data.uri;
+        const reponse = await axios.get(`https://th-api.klaytnapi.com/v2/contract/nft/${nftcontract}/owner/${ownaddress}`, option);
+        // const jsonReponse = JSON.stringify(reponse);
+        const data = reponse.data.items;
+        console.log(data);
+        // let temp = new Array(data.length);
+        let nfts = [];
+        for(let i = 0; i < data.length; i++) {
+            console.log(parseInt(data[i].tokenId,16) );
+            nfts.push({tokenId: parseInt(data[i].tokenId,16), uri: data[i].tokenUri})
+        }
+        console.log(nfts);
+        //console.log(reponse.data);
+        masterNftList();
+        return nfts;
     }catch(e){
         console.log(e);
         return false;
     }
+}
 
-
+export const masterNftList = async () =>{
+    try{
+        const reponse = await axios.get(`https://th-api.klaytnapi.com/v2/contract/nft/${nftcontract}/token`, option);
+        // const jsonReponse = JSON.stringify(reponse);
+        const data = reponse.data.items;
+        console.log(data);
+        // let temp = new Array(data.length);
+        let nfts = [];
+        for(let i = 0; i < data.length; i++) {
+            console.log(parseInt(data[i].tokenId,16) );
+            nfts.push({tokenId: parseInt(data[i].tokenId,16), uri: data[i].tokenUri})
+        }
+        console.log(nfts);
+        //console.log(reponse.data);
+        return nfts;
+    }catch(e){
+        console.log(e);
+        return false;
+    }
 }
