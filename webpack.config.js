af158c78d65029716080e943c44899b09d2f31eb
anxiubin <tnqls0120@naver.com>
2022-02-26 17:05:15 +0900

    ♻️ 불필요한 파일 삭제 및 리드미 업데이트

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ const config = {
   plugins: [
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
     new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] }),
-    new Dotenv({ path: isDevelopment ? './.env.development' : './.env.production', systemvars: true }),
+    new Dotenv({ path: isDevelopment ? './.env.development' : './.env.production' }),
   ],
   output: {
     path: path.join(__dirname, 'dist'),
