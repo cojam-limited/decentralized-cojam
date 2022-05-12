e19a341cbb432076693db73e7a9ab1b586db9f9f
lesile231 <lsang89@naver.com>
2022-05-12 14:27:42 +0900

    remove material-ui/icon

--- components/Navigation/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-export const NavigationContainer = styled.div`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  box-shadow: 0px 0px 14px -3px rgba(0, 0, 0, 0.3);
-`;
